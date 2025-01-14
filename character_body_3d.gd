be5ea4237a4f85a75789b9b6dc0f7d36eee52683
Paul Fok <paul@anthonyfok.org>
2025-01-13 20:15:24 -0700

    Make charge more accurate and the indicator more smooth. 	modified:   character_body_3d.gd 	modified:   charge_indicator.gd

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -61,23 +61,23 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("charge") and charging == 0 and charge_cooldown == 0:
 			$"../Control/ChargeIndicator".charge_start()
 			charge_time = 0.5
-			charge_cooldown = 1.5
+			charge_cooldown = 1
 			
 		if charge_time > 0:
 			charge_time -= 1 * delta
-			if nearwall == 1 and not is_on_floor() and Input.is_action_just_pressed("jump") and nearwallbackward == 1:
+			if not is_on_floor() and Input.is_action_just_pressed("jump") and nearwallbackward == 1:
 				velocity.y = 15
 				var input_dir := Input.get_vector("left", "right", "forward", "backward")
 				var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 				SPEED += 4
-				create_tween().tween_property($".", "SPEED", 5, 2).set_trans(Tween.TRANS_QUART)
-				charging = 0
+				create_tween().tween_property($".", "SPEED", 5, 2).set_trans(Tween.TRANS_CUBIC)
+				charge_time = 0
 				
 		if charge_time <= 0:
 			charge_time = 0
 			charging = 0
 				
-		if charge_cooldown > 0:
+		if charge_cooldown > 0 and not charge_time > 0:
 			charge_cooldown -= 1 * delta
 			if charge_cooldown <= 0:
 				charge_cooldown = 0
@@ -106,13 +106,13 @@ func _physics_process(delta: float) -> void:
 				velocity.z += (direction.z * SPEED * 0.025) #* velocity.z
 			
 		if Input.is_action_just_pressed("down") and is_on_floor() and slide == 0 and abs(velocity.x) + abs(velocity.z) > 5:
-			create_tween().tween_property($Camera3D, "position", Vector3(0,0.35,0), 0.25).set_trans(Tween.TRANS_QUART)
+			create_tween().tween_property($Camera3D, "position", Vector3(0,0.5,0), 0.25).set_trans(Tween.TRANS_CUBIC)
 			var input_dir := Input.get_vector("left", "right", "forward", "backward")
 			var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 			SPEED += 5
 			velocity.x = direction.x * (SPEED)
 			velocity.z = direction.z * (SPEED)
-			create_tween().tween_property($".", "SPEED", 5, 2).set_trans(Tween.TRANS_QUART)
+			create_tween().tween_property($".", "SPEED", 5, 2).set_trans(Tween.TRANS_CUBIC)
 			slide = 1
 			sliding = 1
 			
@@ -122,7 +122,7 @@ func _physics_process(delta: float) -> void:
 				slide = 0
 		
 		if Input.is_action_just_released("down") and slide > 0 or Input.is_action_just_pressed("jump") and slide > 0 or slide == 0 and sliding == 1:
-			create_tween().tween_property($Camera3D, "position", Vector3(0,1.5,0), 0.25).set_trans(Tween.TRANS_QUART)
+			create_tween().tween_property($Camera3D, "position", Vector3(0,1.5,0), 0.25).set_trans(Tween.TRANS_CUBIC)
 			sliding = 0
 			slide = 0
 			
--- charge_indicator.gd
+++ charge_indicator.gd
@@ -8,11 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-		
-	if size.x == 0:
-		create_tween().tween_property($".", "size", Vector2(192,48), 1).set_trans(Tween.TRANS_SINE)
-		create_tween().tween_property($".", "anchors_preset", PRESET_CENTER_BOTTOM, 1).set_trans(Tween.TRANS_SINE)
-		
+	
+	if size.x == 0: #very bad
+		charge_end()
+	
 	if $"../../CharacterBody3D".charge_cooldown == 0 and $".".modulate.a == 1:
 		create_tween().tween_property($".", "modulate:a", 0, .25).set_trans(Tween.TRANS_QUART)
 	pass
@@ -21,3 +20,7 @@ func charge_start():
 	$".".modulate.a = 1
 	create_tween().tween_property($".", "size", Vector2(0,48), 0.5).set_trans(Tween.TRANS_SINE)
 	create_tween().tween_property($".", "anchors_preset", PRESET_CENTER_BOTTOM, 0.5).set_trans(Tween.TRANS_SINE)
+
+func charge_end():
+	create_tween().tween_property($".", "size", Vector2(192,48), 1).set_trans(Tween.TRANS_SINE)
+	create_tween().tween_property($".", "anchors_preset", PRESET_CENTER_BOTTOM, 1).set_trans(Tween.TRANS_SINE)
